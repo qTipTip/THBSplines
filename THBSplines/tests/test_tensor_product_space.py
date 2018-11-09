f384a9d1a6732547ef681c971a88298b503ed96b
Ivar Stangeby <istangeby@gmail.com>
2018-11-09 15:44:01 +0100

    Moved to folder. Got enough intuition to start from scratch now.

--- THBSplines/tests/test_tensor_product_space.py
+++ THBSplines/tests/test_tensor_product_space.py
@@ -24,3 +24,28 @@ def test_univariate_space():
     ]).T
 
     np.testing.assert_allclose(proj, expected_proj)
+
+
+def test_bivariate_space():
+    knots = [
+        [0, 0, 1, 1],
+        [0, 0, 1, 1]
+    ]
+
+    d = [1, 1]
+    dim = 2
+
+    S = TensorProductSpace(d, knots, dim)
+    S2, proj = S.refine()
+
+    expected_proj = np.array([
+        [1., 0., 0., 0.],
+        [0.5, 0.5, 0., 0.],
+        [0., 1., 0., 0.],
+        [0.5, 0., 0.5, 0.],
+        [0.25, 0.25, 0.25, 0.25],
+        [0., 0.5, 0., 0.5],
+        [0., 0., 1., 0.],
+        [0., 0., 0.5, 0.5],
+        [0., 0., 0., 1.]])
+    np.testing.assert_allclose(proj, expected_proj)
