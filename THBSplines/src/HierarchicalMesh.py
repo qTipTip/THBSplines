87b55837b5ef4d8af3337999aaa9374b4fcc38b0
Ivar Stangeby <istangeby@gmail.com>
2018-11-09 14:36:40 +0100

    Moved to folder. Got enough intuition to start from scratch now.

--- THBSplines/src/HierarchicalMesh.py
+++ THBSplines/src/HierarchicalMesh.py
@@ -1,28 +1,30 @@
+from THBSplines.src import TensorProductMesh
+
+
 class HierarchicalMesh(object):
 
-    def __init__(self):
-        self.parametric_dim = 0
-        self.physical_dim = 0
+    def __init__(self, mesh: TensorProductMesh):
+        self.parametric_dim = mesh.parametric_dim
+        self.physical_dim = mesh.physical_dim
 
         self.number_of_levels = 1
-        self.number_of_elements = 0
-        self.number_of_elements_per_level = {}
-        self.active_elements_per_level = {}
-        self.deactivated_elements_per_level = {}
-        self.mesh_per_level = {}
+        self.number_of_elements = mesh.number_of_elements
+        self.number_of_elements_per_level = {0: mesh.number_of_elements}
+        self.active_elements_per_level = {0: set(range(mesh.number_of_elements))}
+        self.deactivated_elements_per_level = {0: set()}
+        self.mesh_per_level = {0: mesh}
 
     def add_new_level(self):
-
         self.number_of_levels = self.number_of_levels + 1
         self.active_elements_per_level[self.number_of_levels] = set()
         self.deactivated_elements_per_level[self.number_of_levels] = set()
         self.number_of_elements_per_level[self.number_of_levels] = 0
-        self.mesh_per_level[self.number_of_levels] = self.refine(self.mesh_per_level[self.self.number_of_levels - 1])
+        self.mesh_per_level[self.number_of_levels] = self.mesh_per_level[self.number_of_levels - 1].refine()
 
     def refine(self):
         pass
 
-    def get_childrne(self):
+    def get_children(self):
         pass
 
     def get_parents(self):
