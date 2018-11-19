083e9b84860e2c942f068e458160d3ec8d4bd9ef
Ivar Stangeby <istangeby@gmail.com>
2018-11-19 16:06:46 +0100

    Plotting working.

--- THBSplines/src/hierarchical_mesh.py
+++ THBSplines/src/hierarchical_mesh.py
@@ -5,7 +5,15 @@ from src.cartesian_mesh import CartesianMesh
 
 class HierarchicalMesh(Mesh):
     def plot_cells(self) -> None:
-        pass
+        import matplotlib.pyplot as plt
+
+        for level in range(self.nlevels):
+            active_cells = self.meshes[level].cells[self.aelem_level[level]]
+            for cell in active_cells:
+                x = cell[0, [0, 1, 1, 0, 0]]
+                y = cell[1, [0, 0, 1, 1, 0]]
+                plt.plot(x, y, color='black')
+        plt.show()
 
     def get_gauss_points(self, cell_indices: np.ndarray) -> np.ndarray:
         pass
@@ -13,8 +21,8 @@ class HierarchicalMesh(Mesh):
     def __init__(self, knots, dim):
         self.meshes = [CartesianMesh(knots, dim)]
         self.nlevels = 1
-        self.aelem_level = {0: np.array((range(self.meshes[0].nelems)))}  # active elements on level
-        self.delem_level = {0: np.array([])}  # deactivated elements on level
+        self.aelem_level = {0: np.array((range(self.meshes[0].nelems)), dtype=np.int)}  # active elements on level
+        self.delem_level = {0: np.array([], dtype=np.int)}  # deactivated elements on level
 
     def add_level(self):
         """
@@ -23,8 +31,8 @@ class HierarchicalMesh(Mesh):
         self.nlevels += 1
         self.meshes.append(self.meshes[-1].refine())
 
-        self.aelem_level[self.nlevels - 1] = (np.array([]))
-        self.delem_level[self.nlevels - 1] = (np.array([]))
+        self.aelem_level[self.nlevels - 1] = (np.array([], dtype=np.int))
+        self.delem_level[self.nlevels - 1] = (np.array([], dtype=np.int))
 
     def refine(self, marked_cells: dict):
         """
@@ -33,8 +41,9 @@ class HierarchicalMesh(Mesh):
         :return:
         """
 
-        if self.nlevels - 1 in marked_cells:
+        while self.nlevels - 1 <= max(marked_cells.keys()):
             self.add_level()
+
         # old_active_cells = self.aelem_level
         new_elements = self.update_active_cells(marked_cells)
 
@@ -53,11 +62,12 @@ class HierarchicalMesh(Mesh):
         for level in range(number_of_levels):
             if level in marked_cells:
                 marked_active_elements = np.where(np.in1d(marked_cells[level], self.aelem_level[level]))
-                self.aelem_level[level] = np.setdiff1d(self.aelem_level[level], marked_active_elements)
-                self.delem_level[level] = np.union1d(self.delem_level[level], marked_cells[level])
+                self.aelem_level[level] = np.setdiff1d(self.aelem_level[level], marked_active_elements).astype(np.int)
+                self.delem_level[level] = np.union1d(self.delem_level[level], marked_cells[level]).astype(np.int)
 
                 new_cells[level + 1] = self.get_children(level, marked_cells[level])
-                self.aelem_level[level + 1] = np.union1d(self.aelem_level[level + 1], new_cells[level + 1])
+                self.aelem_level[level + 1] = np.union1d(self.aelem_level[level + 1], new_cells[level + 1]).astype(
+                    np.int)
 
         self.nel_per_level = [len(self.aelem_level[level]) if level in self.aelem_level else 0 for level in
                               range(self.nlevels)]
@@ -83,5 +93,7 @@ if __name__ == '__main__':
     ]
     dim = 2
     M = HierarchicalMesh(knots, dim)
-    marked_cells = {0: [0, 1, 2, 3]}
-    print(M.refine(marked_cells))
+    marked_cells = {0: [0, 1, 2, 3], 1: [0, 1, 2, 3, 4, 5, 6],
+                    2: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]}
+    M.refine(marked_cells)
+    M.plot_cells()
