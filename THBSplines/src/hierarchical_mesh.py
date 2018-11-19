03ce0c1403dfaf1f2fabdedf5db69855e7a8cf2c
Ivar Stangeby <istangeby@gmail.com>
2018-11-19 15:48:42 +0100

    working on hmesh refinement.

--- THBSplines/src/hierarchical_mesh.py
+++ THBSplines/src/hierarchical_mesh.py
@@ -13,5 +13,72 @@ class HierarchicalMesh(Mesh):
     def __init__(self, knots, dim):
         self.meshes = [CartesianMesh(knots, dim)]
         self.nlevels = 1
-        self.aelem_level = [np.array((range(self.meshes[0].nelems)))]
-        self.delem_level = [np.array([])]
+        self.aelem_level = {0: np.array((range(self.meshes[0].nelems)))}  # active elements on level
+        self.delem_level = {0: np.array([])}  # deactivated elements on level
+
+    def add_level(self):
+        """
+        Adds a new level to the hierarchical mesh, by refining the finest mesh.
+        """
+        self.nlevels += 1
+        self.meshes.append(self.meshes[-1].refine())
+
+        self.aelem_level[self.nlevels - 1] = (np.array([]))
+        self.delem_level[self.nlevels - 1] = (np.array([]))
+
+    def refine(self, marked_cells: dict):
+        """
+        Refines the hierarchical mesh, and returns the global
+        element indices of active elements for each level.
+        :return:
+        """
+
+        if self.nlevels - 1 in marked_cells:
+            self.add_level()
+        old_active_cells = self.aelem_level
+        new_elements = self.update_active_cells(marked_cells)
+
+    def update_active_cells(self, marked_cells):
+        """
+        Updates the set of active cells and deactivated cells.
+        :param marked_cells:
+        :return:
+        """
+
+        number_of_levels = len(marked_cells)
+        new_cells = {}
+
+        for level in range(number_of_levels):
+            print(level)
+            if level in marked_cells:
+                marked_active_elements = np.where(np.in1d(marked_cells[level], self.aelem_level[level]))
+                self.aelem_level[level] = np.setdiff1d(self.aelem_level[level], marked_active_elements)
+                self.delem_level[level] = np.union1d(self.delem_level[level], marked_cells[level])
+
+                new_cells[level + 1] = self.get_children(level, marked_cells[level])
+                self.aelem_level[level + 1] = np.union1d(self.aelem_level[level + 1], new_cells[level + 1])
+
+        self.nel_per_level = [len(self.aelem_level[level]) if level in self.aelem_level else 0 for level in
+                              range(self.nlevels)]
+        self.nel = sum(self.nel_per_level)
+
+    def get_children(self, level: int, marked_cells_at_level) -> np.ndarray:
+        children = np.array([])
+        fine_cells = self.meshes[level + 1].cells
+        for cell_idx in marked_cells_at_level:
+            cell = self.meshes[level].cells[cell_idx]
+            i = np.flatnonzero(np.all((cell[:, 0] <= fine_cells[:, :, 0]) & (cell[:, 1] >= fine_cells[:, :, 1]),
+                                      axis=1))
+            children = np.union1d(children, i)
+        return children
+
+
+if __name__ == '__main__':
+    knots = [
+        [0, 1, 2, 3],
+        [0, 1, 2, 3]
+    ]
+    dim = 2
+    M = HierarchicalMesh(knots, dim)
+    marked_cells = {0: [0, 1, 2, 3]}
+    M.refine(marked_cells)
